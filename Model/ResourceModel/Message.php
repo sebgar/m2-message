e9d9d00ff6a7ce4f1822f7dcb6507965357dc380
sgarrouste <sgarrouste@datasolution.fr>
2021-01-30 14:09:03 +0100

    composer.json + readme

--- Model/ResourceModel/Message.php
+++ Model/ResourceModel/Message.php
@@ -6,6 +6,7 @@ use Magento\Framework\EntityManager\MetadataPool;
 use Magento\Framework\Model\AbstractModel;
 use Magento\Framework\Model\ResourceModel\Db\AbstractDb;
 use Magento\Framework\Model\ResourceModel\Db\Context;
+use Magento\Framework\Stdlib\DateTime\DateTime;
 use Magento\Store\Model\StoreManagerInterface;
 use Sga\Message\Api\Data\MessageInterface as ModelInterface;
 
@@ -14,17 +15,20 @@ class Message extends AbstractDb
     protected $_storeManager;
     protected $_entityManager;
     protected $_metadataPool;
+    protected $_dateTime;
 
     public function __construct(
         Context $context,
         StoreManagerInterface $storeManager,
         EntityManager $entityManager,
         MetadataPool $metadataPool,
+        DateTime $dateTime,
         $connectionName = null
     ) {
         $this->_storeManager = $storeManager;
         $this->_entityManager = $entityManager;
         $this->_metadataPool = $metadataPool;
+        $this->_dateTime = $dateTime;
 
         parent::__construct($context, $connectionName);
     }
@@ -37,13 +41,28 @@ class Message extends AbstractDb
     public function load(AbstractModel $object, $value, $field = null)
     {
         $this->_entityManager->load($object, (int)$value);
+        $this->unpackData($object);
         return $this;
+    }
+
+    public function unpackData(AbstractModel $object)
+    {
     }
 
     public function save(AbstractModel $object)
     {
+        if ($object->isObjectNew()) {
+            $object->setCreatedAt($this->_dateTime->gmtDate());
+        }
+        $object->setUpdatedAt($this->_dateTime->gmtDate());
+
+        $this->packData($object);
         $this->_entityManager->save($object);
         return $this;
+    }
+
+    public function packData(AbstractModel $object)
+    {
     }
 
     public function delete(AbstractModel $object)
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # m2-message
+
+Bandeau messages pour les clients
+
+## Installation
+
+Via composer
+
+```bash
+composer require sebgar/m2-message
+```
